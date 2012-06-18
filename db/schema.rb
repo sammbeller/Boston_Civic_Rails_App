616ada899f879bdba5cd3e2fe693722e016387c1
Jeremy <jcoffman93@yahoo.com>
2012-06-18 14:55:50 -0400

    final commit dummy app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120612200041) do
+ActiveRecord::Schema.define(:version => 20120612201008) do
 
   create_table "reports", :force => true do |t|
     t.float    "latitude"
