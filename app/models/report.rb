9f3a16094d657e29c59e288aadef0baa2fb9b1ad
Rachel Burkhoff <rachelburkhoff@gmail.com>
2012-06-27 13:29:16 -0400

    seeding Settings and Reports

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,6 +5,6 @@ class Report < ActiveRecord::Base
   validates :longitude, presence: true, numericality: true
 
   TIMESTAMP_MIN_TIME = 1341235426528
-  validates :timestamp, presence: true, numericality: true
+  validates :timestamp, presence: true
   belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+#Settings
+
+Setting.create(prompt: "Number of Reports to become trusted user", name: "NumReports", value: 3)
+Setting.create(prompt: "Amount of Updates displayed", name: "HomepageView", value: 10)
+
+puts Setting.count
+puts "*********************111111"
+Report.create(latitude: 42.37021284789698, longitude: -71.03965759277344, timestamp: '2012-06-27 17:22:00.000000')
+Report.create(latitude: 42.354611507112295, longitude: -71.06128692626953, timestamp: '2012-06-22 17:22:00.000000')
+puts "*********************222222"
+puts Report.count
