96bad4dedc30448e4a213bc3d226eb245d170b0b
Jeremy <jcoffman93@yahoo.com>
2012-07-11 16:32:21 -0400

    Changed setup_mail.rb to point to correct domain

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.smtp_settings = {  
   :address              => "smtp.gmail.com",  
   :port                 => 587,  
-  :domain               => "http://electric-galaxy-9820.herokuapp.com",  
+  :domain               => "electric-galaxy-9820.herokuapp.com",  
   :user_name            => "wheresmylane",  
   :password             => "bostoncivicJBS",  
   :authentication       => "plain",  
   :enable_starttls_auto => true  
 }  
-ActionMailer::Base.default_url_options[:host] = "http://electric-galaxy-9820.herokuapp.com"  
+ActionMailer::Base.default_url_options[:host] = "electric-galaxy-9820.herokuapp.com"  
