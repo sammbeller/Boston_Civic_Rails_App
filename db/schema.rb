4b7a5670ab1b1cd9e098d561df24ef8215559a67
sammbeller <sammbeller@gmail.com>
2012-07-17 14:57:57 -0400

    fixed migrtion error

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120715194833) do
-
+ActiveRecord::Schema.define(:version => 20120715194646) do
 
   create_table "loggings", :force => true do |t|
     t.datetime "when"
