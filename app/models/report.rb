10659431248f3a801c6ec8da740087654108c025
Rachel Burkhoff <rachelburkhoff@gmail.com>
2012-07-18 22:26:16 -0400

    seperated heatmap by week days

--- app/models/report.rb
+++ app/models/report.rb
@@ -8,12 +8,6 @@ class Report < ActiveRecord::Base
   validates :timestamp, presence: true
   belongs_to :user
 
-	reverse_geocoded_by :latitude, :longitude do |obj, results|
-		if geo = results.first 
-			obj.city = geo.city
-			obj.address = geo.address
-		end 
-	end 
-
+	reverse_geocoded_by :latitude, :longitude
 	after_validation :reverse_geocode
 end
