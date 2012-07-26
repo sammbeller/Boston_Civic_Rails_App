8a77036f27b2d168e9eb62e508ffdc113a191395
Pito Salas <pitosalas@gmail.com>
2012-07-26 15:15:53 -0400

    fixed ### in .js

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,3 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-
 /* ========================================================
  * bootstrap-tab.js v2.0.4
  * http://twitter.github.com/bootstrap/javascript.html#tabs
