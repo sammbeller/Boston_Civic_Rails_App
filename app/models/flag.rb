e55ccb70f93d256496a21b55b502f2b6395197aa
sammbeller <sammbeller@gmail.com>
2012-07-31 13:25:14 -0400

    fixed error on flag model

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -1,8 +1,17 @@
 class Flag < ActiveRecord::Base
   attr_accessible :cause, :user_id
+  after_save :deactivate_user
 
   validates :cause, presence: true
   validates :user_id, presence: true, uniqueness: {scope: :cause}
 
   belongs_to :user
+
+  private
+
+  def deactivate_user
+  	user = self.user
+  	user.activation = false
+  	user.save
+  end
 end
