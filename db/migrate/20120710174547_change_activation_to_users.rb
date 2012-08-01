5929da78a6cf2ee14e6126578d252b98e89a8296
Jeremy <jcoffman93@yahoo.com>
2012-07-31 21:18:39 -0400

    don't need to be signed in for reports mcreate

--- db/migrate/20120710174547_change_activation_to_users.rb
+++ db/migrate/20120710174547_change_activation_to_users.rb
@@ -1,6 +1,6 @@
 class ChangeActivationToUsers < ActiveRecord::Migration
   def change 
-  	remove_column :users, :actication, :boolean
-    add_column :users, :actication, :boolean, default: false
+  	remove_column :users, :activation
+    add_column :users, :activation, :boolean, default: false
   end
 end
