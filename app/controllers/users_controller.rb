b1bb89bd3dcb2c4965efd660e0ce39d99406372a
sammbeller <sammbeller@gmail.com>
2012-08-01 12:11:41 -0400

    added case for taken emails on mcreate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,18 +70,22 @@ class UsersController < ApplicationController
   def mcreate
     puts "******************* #{params.inspect}"
     email = params[:email]
-    puts params.inspect
-    pw = SecureRandom.urlsafe_base64
-    @user = User.new(email: email, password: pw, password_confirmation: pw)
-    Logging.create(when: (DateTime.now), user_id: current_user, event: "Requesting new Account" )
+    if user.find_by_email(email)
+      respond_to do |format|
+        format.json { render json: { reponse: "Email already taken!" }
+    else
+      pw = SecureRandom.urlsafe_base64
+      @user = User.new(email: email, password: pw, password_confirmation: pw)
+      Logging.create(when: (DateTime.now), user_id: current_user, event: "Requesting new Account" )
 
-    respond_to do |format|
-      if @user.save
-        # Tell the UserMailer to send a welcome Email after save
-        UserMailer.activation_email(@user).deliver
-        format.json { render json: {token: @user.remember_token, NumReports: Setting.find(1).value}, status: :created }
-      else
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @user.save
+          # Tell the UserMailer to send a welcome Email after save
+          UserMailer.activation_email(@user).deliver
+          format.json { render json: {token: @user.remember_token, NumReports: Setting.find(1).value}, status: :created }
+        else
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
